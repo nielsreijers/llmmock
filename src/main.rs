970f1af011530cf9c5efff80af6a05272187b7ce
Niels Reijers <github@mail.nielsreijers.com>
2026-08-08 13:25:34 +0800

    Add --port CLI parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::println;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+    #[arg(short = 'p', long = "port", default_value = "8080")]
+    pub port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("Listening on port {}...", cli.port);
 }
