bcae23d2f702689ade19abbbe666982aa35a19ed
Niels Reijers <github@mail.nielsreijers.com>
2026-08-09 22:53:56 +0800

    Simplify enums

--- src/openai/req.rs
+++ src/openai/req.rs
@@ -14,12 +14,35 @@ pub(crate) struct ChatCompletionRequest {
 #[serde(tag = "role")]
 #[serde(rename_all = "snake_case")]
 pub(crate) enum ChatCompletionMessageParam {
-    #[allow(unused)] Developer(ChatCompletionDeveloperMessageParam),
-    #[allow(unused)] System(ChatCompletionSystemMessageParam),
-    #[allow(unused)] User(ChatCompletionUserMessageParam),
-    #[allow(unused)] Assistant(ChatCompletionAssistantMessageParam),
-    #[allow(unused)] Tool(ChatCompletionToolMessageParam),
-    #[allow(unused)] Function(ChatCompletionFunctionMessageParam),
+    #[allow(unused)] Developer {
+        content: ChatCompletionContentTextOnly,
+        name: Option<String>,
+    },
+    #[allow(unused)] System {
+        content: ChatCompletionContentTextOnly,
+        name: Option<String>,
+    },
+    #[allow(unused)] User {
+        content: ChatCompletionContent,
+        name: Option<String>,
+    },
+    #[allow(unused)] Assistant {
+        audio: Option<Audio>,
+        content: Option<ChatCompletionContentTextOrRefusal>,
+        #[deprecated(note = "replaced by tool_calls")]
+        function_call: Option<FunctionCall>,
+        name: Option<String>,
+        refusal: Option<String>,
+        tool_calls: Option<Vec<ChatCompletionMessageToolCall>>,
+    },
+    #[allow(unused)] Tool {
+        content: ChatCompletionContentTextOnly,
+        tool_call_id: String,
+    },
+    #[allow(unused)] Function {
+        content: Option<String>,
+        name: String,
+    },
 }
 
 #[derive(Debug, Deserialize)]
@@ -53,16 +76,22 @@ pub(crate) enum ChatCompletionContent {
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 pub(crate) enum ChatCompletionContentPart {
-    #[allow(unused)] Text(ChatCompletionContentPartText),
-    #[allow(unused)] ImageUrl(ChatCompletionContentPartImage),
-    #[allow(unused)] InputAudio(ChatCompletionContentPartInputAudio),
-    #[allow(unused)] File(FileContentPart),
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionContentPartText {
-    #[allow(unused)] pub text: String,
-    #[allow(unused)] pub prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
+    #[allow(unused)] Text {
+        text: String,
+        prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
+    },
+    #[allow(unused)] ImageUrl {
+        image_url: ImageUrl,
+        prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
+    },
+    #[allow(unused)] InputAudio {
+        input_audio: InputAudio,
+        prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
+    },
+    #[allow(unused)] File {
+        file: File,
+        prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
+    },
 }
 
 #[derive(Debug, Deserialize)]
@@ -79,12 +108,6 @@ pub(crate) enum ImageUrlDetail {
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionContentPartImage {
-    #[allow(unused)] pub image_url: ImageUrl,
-    #[allow(unused)] pub prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
-}
-
-#[derive(Debug, Deserialize)]
 pub(crate) struct InputAudio {
     #[allow(unused)] pub data: String,
     #[allow(unused)] pub format: InputAudioFormat,
@@ -97,27 +120,10 @@ pub(crate) enum InputAudioFormat {
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionContentPartInputAudio {
-    #[allow(unused)] pub input_audio: InputAudio,
-    #[allow(unused)] pub prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
-}
-
-#[derive(Debug, Deserialize)]
 pub(crate) struct File {
     #[allow(unused)] pub file_data: Option<String>,
     #[allow(unused)] pub file_id: Option<String>,
     #[allow(unused)] pub filename: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct FileContentPart {
-    #[allow(unused)] pub file: File,
-    #[allow(unused)] pub prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionContentPartRefusal {
-    #[allow(unused)] pub refusal: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -129,8 +135,13 @@ pub(crate) struct Audio {
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 pub(crate) enum ChatCompletionContentPartTextOrRefusal {
-    #[allow(unused)] Text(ChatCompletionContentPartText),
-    #[allow(unused)] Refusal(ChatCompletionContentPartRefusal),
+    #[allow(unused)] Text {
+        text: String,
+        prompt_cache_breakpoint: Option<ChatCompletionPromptCacheBreakpoint>,
+    },
+    #[allow(unused)] Refusal {
+        refusal: String,
+    },
 }
 
 #[derive(Debug, Deserialize)]
@@ -139,35 +150,10 @@ pub(crate) enum ChatCompletionContentTextOrRefusal {
     #[allow(unused)] Text(String),
     #[allow(unused)] Structured(Vec<ChatCompletionContentPartTextOrRefusal>),
 }
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionDeveloperMessageParam {
-    #[allow(unused)] pub content: ChatCompletionContentTextOnly,
-    #[allow(unused)] pub name: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionSystemMessageParam {
-    #[allow(unused)] pub content: ChatCompletionContentTextOnly,
-    #[allow(unused)] pub name: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionUserMessageParam {
-    #[allow(unused)] pub content: ChatCompletionContent,
-    #[allow(unused)] pub name: Option<String>,
-}
-
 #[derive(Debug, Deserialize)]
 pub(crate) struct FunctionCall {
     #[allow(unused)] pub arguments: String,
     #[allow(unused)] pub name: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct FunctionToolCall {
-    #[allow(unused)] pub id: String,
-    #[allow(unused)] pub function: FunctionCall,
 }
 
 #[derive(Debug, Deserialize)]
@@ -177,41 +163,17 @@ pub(crate) struct CustomCall {
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct CustomToolCall {
-    #[allow(unused)] pub id: String,
-    #[allow(unused)] pub custom: CustomCall,
-}
-
-#[derive(Debug, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 pub(crate) enum ChatCompletionMessageToolCall {
-    #[allow(unused)] Function(FunctionToolCall),
-    #[allow(unused)] Custom(CustomToolCall),
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionAssistantMessageParam {
-    #[allow(unused)] pub audio: Option<Audio>,
-    #[allow(unused)] pub content: Option<ChatCompletionContentTextOrRefusal>,
-    #[deprecated(note = "replaced by tool_calls")]
-    #[allow(unused)]
-    pub function_call: Option<FunctionCall>,
-    #[allow(unused)] pub name: Option<String>,
-    #[allow(unused)] pub refusal: Option<String>,
-    #[allow(unused)] pub tool_calls: Option<Vec<ChatCompletionMessageToolCall>>,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionToolMessageParam {
-    #[allow(unused)] pub content: ChatCompletionContentTextOnly,
-    #[allow(unused)] pub tool_call_id: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionFunctionMessageParam {
-    #[allow(unused)] pub content: Option<String>,
-    #[allow(unused)] pub name: String,
+    #[allow(unused)] Function {
+        id: String,
+        function: FunctionCall,
+    },
+    #[allow(unused)] Custom {
+        id: String,
+        custom: CustomCall,
+    },
 }
 
 #[derive(Debug, Deserialize)]
@@ -252,15 +214,12 @@ pub(crate) enum ChatCompletionAudioParamVoiceBuiltin {
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct ChatCompletionAudioParamVoiceCustom {
-    #[allow(unused)] id: String,
-}
-
-#[derive(Debug, Deserialize)]
 #[serde(untagged)]
 pub(crate) enum ChatCompletionAudioParamVoice {
     #[allow(unused)] Builtin(ChatCompletionAudioParamVoiceBuiltin),
-    #[allow(unused)] Custom(ChatCompletionAudioParamVoiceCustom),
+    #[allow(unused)] Custom {
+        id: String,
+    },
 }
 
 #[derive(Debug, Deserialize)]
